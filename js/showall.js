d2ccdf992b61cbb38a1ac8390b72e63799c67783
sidney <shogo12000@hotmail.com>
2024-02-23 16:56:05 -0800

    update search

--- js/showall.js
+++ js/showall.js
@@ -1,6 +1,57 @@
 import { allMovies} from '../js/common.js'
 import { showResult } from '../js/search.js'
 
+let filterMovies = document.getElementById("filterMovies")
+let filterTv     = document.getElementById("filterTv")
+let search_field = document.getElementById("search_field")
+
+
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTA0NDM0NmI0NmFlYzc2OTQ3YjdiMTJjMjJmMDM3YiIsInN1YiI6IjY1YTFiOWIyOWFlNjEzMDEyZWI3NzQ5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xhyB4idFFjVhxXuovbo9sMLpO-4mqhTcPy_C04WIIsg'
+    }
+  };
+  
+
+
+filterAll.addEventListener("click",(e)=>{
+    if(search_field.value != ""){
+        fetch('https://api.themoviedb.org/3/search/multi?query='+search_field.value+'&include_adult=false&language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response =>{ showResult(response); })
+        .catch(err => console.error(err));
+    }else{
+        noFilter();
+    }
+})
+
+
+filterMovies.addEventListener("click",(e)=>{
+    if(search_field.value != ""){
+        fetch('https://api.themoviedb.org/3/search/movie?query='+search_field.value+'&include_adult=false&language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response =>{ showResult(response); })
+        .catch(err => console.error(err));
+    }else{
+        noFilter();
+    }
+})
+
+filterTv.addEventListener("click",(e)=>{
+    if(search_field.value != ""){
+        fetch('https://api.themoviedb.org/3/search/tv?query='+search_field.value+'&include_adult=false&language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response =>{ showResult(response); })
+        .catch(err => console.error(err));
+    }else{
+        noFilter();
+    }
+})
+
+
 
 
 // Get the query string from the URL
@@ -15,12 +66,30 @@ console.log(user_search); // shirt
 
 btnSearch;
 
-allMovies(user_search,1)
-    .then((e)=>e.json())
-    .then(res=>{
-        console.log(res)
-        document.querySelector("main").style.display="block";
-        showResult(res);
 
 
-    })
+
+
+
+    allMovies(user_search,1)
+        .then((e)=>e.json())
+        .then(res=>{
+            console.log(res)
+            document.querySelector("main").style.display="block";
+            showResult(res);
+
+
+        })
+
+
+    function noFilter(){
+        allMovies(user_search,1)
+        .then((e)=>e.json())
+        .then(res=>{
+            console.log(res)
+            document.querySelector("main").style.display="block";
+            showResult(res);
+
+
+        })
+    }
