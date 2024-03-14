c657099d0be14758be263cda1b566037455594f3
Rafael Ferreira <rafaelmfer@hotmail.com>
2024-03-14 03:16:26 -0700

    Create a new carousel and implementing on friends page

--- js/friends.js
+++ js/friends.js
@@ -1,11 +1,6 @@
 import { checkSession } from "./auth.js";
-import {
-    getInfoDb,
-    getAllDocsInSubcollection,
-    getDocsByQuery,
-} from "./firestore.js";
-
-import { getDoc } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+import { getInfoDb, getDocsByQuery } from "./firestore.js";
+import { createCarousel, initializeCarousel } from "./common.js";
 
 let mainFriends = document.getElementById("main-Friends");
 let mainDiscover = document.getElementById("mainDiscover");
@@ -18,16 +13,13 @@ activeFriends.addEventListener("click", () => {
     mainFriends.classList.add("active");
     mainDiscover.classList.add("notActive");
     mainDiscover2.classList.add("notActive");
-    document.getElementById("test").classList.add("hidden");
 });
 
 activeShowsMovies.addEventListener("click", () => {
     mainFriends.classList.remove("active");
     mainDiscover.classList.remove("notActive");
     mainDiscover2.classList.add("notActive");
-    document.getElementById("test").classList.remove("hidden");
 });
-
 
 const user = await checkSession();
 let documentId = user.uid;
@@ -40,44 +32,43 @@ processFriends();
 
 async function processFriends() {
     for (const friendRef of documentDb.friends) {
-        const friendFirestore = await getDoc(friendRef.reference);
-        const friendDoc = friendFirestore.data();
+        const friendDoc = await getInfoDb(friendRef.reference.path);
 
         let watchlistPath = `${friendRef.reference.path}/watchlist`;
-        let watchlistArray = await getAllDocsInSubcollection(watchlistPath, {});
+        let watchlistArrayNotCompleted = await getDocsByQuery(
+            watchlistPath,
+            "completed",
+            "==",
+            false,
+            {}
+        );
 
-        let notCompleted = watchlistArray.filter(function (item) {
-            if (item.completed !== true) {
-                return item;
-            }
-        });
-
-        createSectionWithFilms(friendDoc.name, notCompleted);
+        createSectionWithFilms(friendDoc.name, watchlistArrayNotCompleted);
     }
 }
 
 function createSectionWithFilms(name, films) {
-    // Criação da seção
-    var section = document.createElement('section');
-    section.classList.add('example');
+    // Creating the section
+    var section = document.createElement("section");
+    section.classList.add("component-friends-watchlist");
 
-    // Criação do cabeçalho da seção
-    var headerDiv = document.createElement('div');
-    headerDiv.classList.add('section-header');
+    // Creating the section header
+    var headerDiv = document.createElement("div");
+    headerDiv.classList.add("section-header");
 
-    var nameHeader = document.createElement('h2');
+    var nameHeader = document.createElement("h2");
     nameHeader.textContent = `${name}'s Watchlist`;
 
-    var viewProfileDiv = document.createElement('div');
-    viewProfileDiv.classList.add('view-profile');
+    var viewProfileDiv = document.createElement("div");
+    viewProfileDiv.classList.add("view-profile");
 
-    var viewProfileText = document.createElement('p');
-    viewProfileText.textContent = 'View Profile';
+    var viewProfileText = document.createElement("p");
+    viewProfileText.textContent = "View Profile";
 
-    var arrowsImg = document.createElement('img');
-    arrowsImg.classList.add('arrows');
-    arrowsImg.src = '../resources/icons/icons-default/default-arrow-right.svg';
-    arrowsImg.alt = 'image';
+    var arrowsImg = document.createElement("img");
+    arrowsImg.classList.add("arrows");
+    arrowsImg.src = "../resources/icons/icons-default/default-arrow-right.svg";
+    arrowsImg.alt = "arrow-right";
 
     viewProfileDiv.appendChild(viewProfileText);
     viewProfileDiv.appendChild(arrowsImg);
@@ -85,144 +76,49 @@ function createSectionWithFilms(name, films) {
     headerDiv.appendChild(nameHeader);
     headerDiv.appendChild(viewProfileDiv);
 
-    // Criação do conteúdo da seção
-    var contentDiv = document.createElement('div');
-    contentDiv.classList.add('section-content');
+    // Creating the content of the section
+    var contentDiv = document.createElement("div");
+    contentDiv.classList.add("section-content");
 
-    // Adicionando filmes
-    films.forEach(function(film) {
-        var filmDiv = document.createElement('div');
-        filmDiv.classList.add('film-serie');
+    // Creating the carousel
+    var carousel = createCarousel(films, createCarouselItem);
+    contentDiv.appendChild(carousel.container);
 
-        var link = document.createElement('a');
-        if (film.media_type == "movie") {
-            link.setAttribute("href", "single_movie.html?id=" + film.id);
-        } else {
-            link.setAttribute("href", "single_series.html?id=" + film.id);
-        }
-
-        var img = document.createElement('img');
-        img.classList.add('movie-series-placeholder');
-        img.src = "http://image.tmdb.org/t/p/w154" + film.poster_path;
-        // img.alt = film.alt;
-        // img.width = film.width;
-        // img.height = film.height;
-
-        var p = document.createElement('p');
-        p.textContent = film.name || film.title;
-
-        link.appendChild(img);
-        link.appendChild(p);
-
-        filmDiv.appendChild(link);
-
-        contentDiv.appendChild(filmDiv);
-    });
-
-    // Adicionando cabeçalho e conteúdo à seção
+    // Adding header and content to the section
     section.appendChild(headerDiv);
     section.appendChild(contentDiv);
 
-    // Adicionando a seção ao elemento com id "main-Friends"
-    var mainFriends = document.getElementById('main-Friends');
+    // Adding the section to the main element
+    var mainFriends = document.getElementById("main-Friends");
     mainFriends.appendChild(section);
+
+    // Initialize carousel
+    initializeCarousel(carousel);
 }
 
-for(let x=0; x<2; x++){
-    let res = [...myObj];
-    createCarousel(res);
-}
+function createCarouselItem(film) {
+    var filmDiv = document.createElement("div");
+    filmDiv.classList.add("item");
 
-//CREATE FRIENDS CAROUSEL
-async function createCarousel(friend) {
-    await checkSe()
-    console.log("=========================")
-    console.log(user)
-    // Create section element
-    var section = document.createElement("section");
-    section.className = "section_movies";
-
-    // Create div.div_movies element
-    var divMovies = document.createElement("div");
-    divMovies.className = "div_movies";
-
-    // Create div.movies_title_h2 element
-    var divTitle = document.createElement("div");
-    divTitle.className = "movies_title_h2";
-
-    // Create h2 element
-    var h2 = document.createElement("h2");
-    h2.textContent = amigo.name;
-
-    // Create span element
-    var span = document.createElement("span");
-    var a = document.createElement("a");
-    a.href = "#";
-    a.textContent = "See More ";
-    span.appendChild(a);
-    h2.appendChild(span);
-    divTitle.appendChild(h2);
-
-    // Append div.movies_title_h2 to div.div_movies
-    divMovies.appendChild(divTitle);
-
-    // Create div.swiper.mySwiper.mySwiper1 element
-    var divSwiper = document.createElement("div");
-    divSwiper.className = "swiper mySwiper mySwiper1";
-
-    // Create div.swiper-wrapper element
-    var divSwiperWrapper = document.createElement("div");
-    divSwiperWrapper.className = "swiper-wrapper";
-
-    // Create multiple swiper-slide elements
-    for (var i = 0; i < friend.length; i++) {
-        var swiperSlide = document.createElement("div");
-        swiperSlide.className = "swiper-slide";
-
-        var documentaryImg = document.createElement("div");
-        documentaryImg.className = "documentary_img4";
-
-        var a = document.createElement("a");
-        a.href = "single_movie.html?id=1011985";
-
-        var divImage = document.createElement("div");
-        divImage.className = "div_image";
-
-        var img = document.createElement("img");
-        img.src = "https://image.tmdb.org/t/p/original" + friend[i].poster_path;
-
-        var p = document.createElement("p");
-        p.textContent = "Kung Fu Panda 4";
-
-        divImage.appendChild(img);
-        divImage.appendChild(p);
-        a.appendChild(divImage);
-        documentaryImg.appendChild(a);
-        swiperSlide.appendChild(documentaryImg);
-        divSwiperWrapper.appendChild(swiperSlide);
+    var link = document.createElement("a");
+    if (film.media_type == "movie") {
+        link.setAttribute("href", "single_movie.html?id=" + film.id);
+    } else {
+        link.setAttribute("href", "single_series.html?id=" + film.id);
     }
 
-    // Append div.swiper-wrapper to div.swiper.mySwiper.mySwiper1
-    divSwiper.appendChild(divSwiperWrapper);
+    var img = document.createElement("img");
+    img.classList.add("movie-series-placeholder");
+    img.src = "http://image.tmdb.org/t/p/w154" + film.poster_path;
+    img.alt = film.name || film.title;
 
-    // Create div.swiper-button-next and div.swiper-button-prev elements
-    var divNextButton = document.createElement("div");
-    divNextButton.className = "swiper-button-next";
+    var p = document.createElement("p");
+    p.textContent = film.name || film.title;
 
-    var divPrevButton = document.createElement("div");
-    divPrevButton.className = "swiper-button-prev";
+    link.appendChild(img);
+    link.appendChild(p);
 
-    // Append div.swiper-button-next and div.swiper-button-prev to div.swiper.mySwiper.mySwiper1
-    divSwiper.appendChild(divNextButton);
-    divSwiper.appendChild(divPrevButton);
+    filmDiv.appendChild(link);
 
-    // Append div.swiper.mySwiper.mySwiper1 to div.div_movies
-    divMovies.appendChild(divSwiper);
-
-    // Append div.div_movies to section.section_movies
-    section.appendChild(divMovies);
-
-    if (mainFriends) {
-        mainFriends.appendChild(section);
-    }
+    return filmDiv;
 }
