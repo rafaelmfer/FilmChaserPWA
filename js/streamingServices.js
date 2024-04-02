a6049b9b30603b400d13ecc97bb19a8b2344ec55
Rafael Ferreira <rafaelmfer@hotmail.com>
2024-04-01 23:08:04 -0700

    Fix styling layout, create button skip function, and fix button next function

--- js/streamingServices.js
+++ js/streamingServices.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import { updateInfoDb } from "../js/firestore.js";
 import { checkSession } from "./auth.js";
@@ -7,53 +7,67 @@ import { networkInfo } from "./common.js";
 networkInfo();
 
 // Selecting all checkboxes
-const checkboxes = document.querySelectorAll('.checkbox');
+const checkboxes = document.querySelectorAll(".checkbox");
 
 // Selecting the element to display the selected count
-const selectedCountElement = document.querySelector('.streaming-title h6');
+const selectedCountElement = document.querySelector(".streaming-title h6");
 
 // Function to update the selected count
 function updateSelectedCount() {
-    const selectedCount = document.querySelectorAll('.checkbox.checked').length;
+    const selectedCount = document.querySelectorAll(".checkbox.checked").length;
     selectedCountElement.textContent = `${selectedCount} Selected`;
 }
 
 // Adding an event listener to each checkbox
 checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('click', function() {
+    checkbox.addEventListener("click", function () {
         // Toggling the 'checked' class to change the checkbox image
-        this.classList.toggle('checked');
+        this.classList.toggle("checked");
         // Update the selected count
         updateSelectedCount();
     });
 });
 
 // Selecting the "Next" button
-const btnNext = document.querySelector('.btn-next');
+const btnNext = document.querySelectorAll(".btn-next");
 
 // Adding an event listener to the "Next" button
-btnNext.addEventListener('click', async function() {
-    // Array to store selected services
-    const selectedServices = [];
-    checkboxes.forEach((checkbox) => {
-        if (checkbox.classList.contains('checked')) {
-            // Getting the service name associated with the checkbox
-            const serviceName = checkbox.closest('.streaming-service-container').querySelector('.body-text').textContent;
-            selectedServices.push(serviceName);
-        }
+btnNext.forEach((button) => {
+    button.addEventListener("click", async function () {
+        // Array to store selected services
+        const selectedServices = [];
+        checkboxes.forEach((checkbox) => {
+            if (checkbox.classList.contains("checked")) {
+                // Getting the service name associated with the checkbox
+                const serviceName = checkbox
+                    .closest(".streaming-service-container")
+                    .querySelector(".body-text").textContent;
+                selectedServices.push(serviceName);
+            }
+        });
+
+        // Checking the user session
+        const user = await checkSession();
+        let documentId = user.uid;
+
+        // Updating the user info in the Firebase database
+        await updateInfoDb(`users/${documentId}`, {
+            streamingServices: selectedServices,
+        });
+
+        // Logging selected services to console
+        console.log(selectedServices);
+
+        window.location.replace("genres.html");
     });
+});
 
-    // Checking the user session
-    const user = await checkSession();
-    let documentId = user.uid;
+// Selecting the "Skip" button
+const btnSkip = document.querySelectorAll(".btn-skip");
 
-    // Updating the user info in the Firebase database
-    await updateInfoDb(`users/${documentId}`, {
-        streamingServices: selectedServices,
+// Adding an event listener to the "Skip" button
+btnSkip.forEach((button) => {
+    button.addEventListener("click", function () {
+        window.location.replace("genres.html");
     });
-
-    // Logging selected services to console
-    console.log(selectedServices);
-
-    window.location.replace("genres.html");
 });
