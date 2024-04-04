f9a930a89a8f50c2c356e034b33a93893ef74d59
Rafael Ferreira <rafaelmfer@hotmail.com>
2024-04-04 07:47:46 -0700

    setup for vercel deployment

--- z_ext_libs/themoviedb/themoviedb.js
+++ z_ext_libs/themoviedb/themoviedb.js
@@ -12,8 +12,8 @@ var theMovieDb = {};
 
 theMovieDb.common = {
   api_key: API_KEY_TMDB,
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "en-US",
   include_adult: false,
